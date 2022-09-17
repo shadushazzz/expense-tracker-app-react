2801690c37589d8cddc01e4b5025790624b7712b
Shadab <Shadab>
2022-09-17 17:21:52 +0530

    added dynamic list from forms

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -10,29 +10,20 @@ const Expenses = (props) => {
   const receivedFilterYear = ( receivedYear ) => {
     setFilterYear(receivedYear);
   }
+
+  const filteredList = props.items.filter(item => item.date.getFullYear() === Number(filterYear));
+ 
   return (
   <Card className='expenses'>
     <ExpensesFilter selected={filterYear} onExpenseFilterChange={receivedFilterYear} />
-    <ExpenseItems
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItems>
-      <ExpenseItems
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItems>
-      <ExpenseItems
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItems>
-      <ExpenseItems
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItems>
+    {
+      filteredList.map( item => 
+        <ExpenseItems
+        title={item.title}
+        amount={item.amount}
+        date={item.date} />
+      )
+    }    
   </Card>
   )
 }
